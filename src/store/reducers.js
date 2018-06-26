fbec684f04ad88de0acc42893fcddf4c428c5322
Pablo Anttila <pablo.anttila@tobii.com>
2018-06-26 10:08:51 +0200

    reducers naming fixed

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -3,7 +3,8 @@ import { combineReducers } from 'redux';
 //import individual reducers here
 import userLocationReducer from './reducers/userLocation';
 import getCategories from './reducers/categories';
+import getSkills from './reducers/skills';
 
 export default combineReducers({
-  userLocationReducer, getCategories
+  userLocationReducer, getCategories, getSkills
 })
