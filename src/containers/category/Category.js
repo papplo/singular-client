1abfe47bd0345bfa5f1b548eddebc9c1f1946e9b
Pablo Anttila <pablo.anttila@tobii.com>
2018-06-28 13:25:05 +0200

    feat: filter skills array in skillView, first markup for skillProfile

--- src/containers/category/Category.js
+++ src/containers/category/Category.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Hero, BulmaBoiler, TagCloud, ActivityList } from '../../components/'
+
 import './Category.css';
 import pathParser from '../../services/pathparser'
 import categoryId from '../../services/categoryId'
@@ -12,7 +15,7 @@ class Category extends Component {
       initialize: false,
     }
   }
-  
+
   componentWillMount() {
     if (this.props.categories.status === 200 && this.props.userLocation.status === 200 && !this.state.initialize) this.initialize();
   }
@@ -25,26 +28,61 @@ class Category extends Component {
     this.setState({initialize: true})
     const categoryName = await pathParser(this.props.location.pathname);
     if (!categoryName.name) return;
+    this.setState({category: categoryName.name});
     const categoryID = await categoryId(this.props.categories.body, categoryName.name);
     this.props.genreSkills(this.props.userLocation, categoryID)
   }
 
-  //CAN DELETE THIS --- JUST EXAMPLE OF RENDERING SKILLS IN GIVEN CATEGORY -----
-  renderSkills = () => {
-    console.log('skills: ', this.props.skills);
-    if (this.props.skills.status !== 200) return null;
-    return this.props.skills.body.map(skill => {
-      return <p>{skill.title}</p>
-    })
+  filterList = (event) => {
+    // make spinner loader appear
+    event.currentTarget.classList.add('is-loading');
+    let updatedList = this.props.skills.body;
+
+    updatedList = updatedList.filter((item) => {
+      return item.title.toLowerCase().search(
+        event.target.value.toLowerCase()) !== -1;
+    });
+    console.log(updatedList.length);
+    this.setState({filteredList: {
+      status: 200,
+      body: updatedList
+    }});
+    event.currentTarget.classList.remove('is-loading');
   }
-  //-----------------------------------------------------------------------------
+
+  clearSearch = (event) => {
+    // clear search in box
+    console.log(event.target);
+  }
 
   render () {
     return (
       <div>
-        <p>Category Page </p>
-        {/* CAN DELETE RENDERSKILLS(), JUST EXAMPLE */}
-        {this.renderSkills()}
+        <section className="section is-center has-stroke-bottom has-half-padding-bottom">
+          <div className="container">
+            <p> Put your dance shoes on!</p>
+            <h1 className="title is-capitalized">
+              {this.state.category}
+            </h1>
+          </div>
+        </section>
+        <ActivityList skills={this.state.filteredList || this.props.skills}>
+          <div className="field">
+              <div className="control has-icons-right is-medium"
+                onChange={(e) => this.filterList(e)}>
+                <input type="search" className="input is-medium" type="email" placeholder="Search for a tag, name or person" />
+                <span className="icon is-small is-left">
+                  <i className="fas fa-envelope"></i>
+                </span>
+                <span className="icon is-small is-right" onClick={(e) => this.clearSearch(e)}>
+                  <i className="fas fa-check">x</i>
+                </span>
+              </div>
+          </div>
+          <p className="subtitle">
+            See whats´s offered in {this.props.userLocation.status === 200 && this.props.userLocation.body.city}
+          </p>
+        </ActivityList>
       </div>
     )
   }
