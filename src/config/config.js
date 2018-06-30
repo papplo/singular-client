45c77e0285b24226306244f158442bd59862ebf6
Pablo Anttila <pablo.anttila@tobii.com>
2018-06-30 18:23:51 +0200

    FEAT: breaking changes for async lists, all constants in config-file now

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,9 @@
 export default {
-  server_url: 'http://localhost',
+  server_url: 'http://192.168.0.19',
   server_port: ':3010',
   geolookup_url: 'https://json.geoiplookup.io',
+  STATUS_NO_DATA : 'unloaded',
+  STATUS_LOADING : 'loading',
+  STATUS_FAILURE : 'failure',
+  STATUS_OK : 200,
 }
