e451ec77f3c9b329cace0f8449175d3029a5bb1e
davidecorreu <davidecorreu@gmail.com>
2018-07-04 15:31:12 +0200

    Update locationReducer.js

--- src/store/reducers/locationReducer.js
+++ src/store/reducers/locationReducer.js
@@ -1,29 +1,31 @@
-// const location = (state = {status: 'unloaded', body: { city: ''}}, action) => {
-//   switch (action.type) {
-//     case 'FETCH_LOCATION':
-//         return {status: 'loading'};
-//     case 'FETCH_LOCATION_FAILURE':
-//         return {status: 'failure'};
-//     case 'FETCH_LOCATION_SUCCESS':
-//         return action.responseObject;
-//     default:
-//       return state;
-//   }
-// }
-//
-// export default location;
-
-const location = (state = {status: 'unloaded', body: ''}, action) => {
-    switch (action.type) {
-      case 'FETCH_LOCATION':
-          return {status: 'loading'};
-      case 'FETCH_LOCATION_FAILURE':
-          return {status: 200, body: {city: 'Barcelona'}}
-      case 'FETCH_LOCATION_SUCCESS':
-          return {status: 200, body: {city: 'Barcelona'}};
-      default:
-        return state;
-    }
+const location = (state = {status: 'unloaded', body: { city: ''}}, action) => {
+  switch (action.type) {
+    case 'FETCH_LOCATION':
+        return {status: 'loading'};
+    case 'FETCH_LOCATION_FAILURE':
+        return {status: 'failure'};
+    case 'FETCH_LOCATION_SUCCESS':
+        return action.responseObject;
+    default:
+      return state;
   }
+}
 
 export default location;
+
+// Use this code if the geolocation API is not responding
+// 
+// const location = (state = {status: 'unloaded', body: ''}, action) => {
+//     switch (action.type) {
+//       case 'FETCH_LOCATION':
+//           return {status: 'loading'};
+//       case 'FETCH_LOCATION_FAILURE':
+//           return {status: 200, body: {city: 'Barcelona'}}
+//       case 'FETCH_LOCATION_SUCCESS':
+//           return {status: 200, body: {city: 'Barcelona'}};
+//       default:
+//         return state;
+//     }
+//   }
+
+// export default location;
