32716bfa08b6ab9531f4eb7e2599ddb61527e7d2
cristinagf <37103989+cristinaguimaraes@users.noreply.github.com>
2018-07-04 18:08:47 +0200

    Update locationReducer.js

--- src/store/reducers/locationReducer.js
+++ src/store/reducers/locationReducer.js
@@ -1,27 +1,27 @@
-// const location = (state = {status: 'unloaded', body: { city: ''}}, action) => {
-//   switch (action.type) {
-//     case 'FETCH_LOCATION':
-//         return {status: 'loading'};
-//     case 'FETCH_LOCATION_FAILURE':
-//         return {status: 'failure'};
-//     case 'FETCH_LOCATION_SUCCESS':
-//         return action.responseObject;
-//     default:
-//       return state;
-//   }
-// }
-
-const location = (state = {status: 'unloaded', body: ''}, action) => {
-    switch (action.type) {
-      case 'FETCH_LOCATION':
-          return {status: 'loading'};
-      case 'FETCH_LOCATION_FAILURE':
-          return {status: 200, body: {city: 'Barcelona'}}
-      case 'FETCH_LOCATION_SUCCESS':
-          return {status: 200, body: {city: 'Barcelona'}};
-      default:
-        return state;
-    }
+const location = (state = {status: 'unloaded', body: { city: ''}}, action) => {
+  switch (action.type) {
+    case 'FETCH_LOCATION':
+        return {status: 'loading'};
+    case 'FETCH_LOCATION_FAILURE':
+        return {status: 'failure'};
+    case 'FETCH_LOCATION_SUCCESS':
+        return action.responseObject;
+    default:
+      return state;
   }
+}
+
+// const location = (state = {status: 'unloaded', body: ''}, action) => {
+//     switch (action.type) {
+//       case 'FETCH_LOCATION':
+//           return {status: 'loading'};
+//       case 'FETCH_LOCATION_FAILURE':
+//           return {status: 200, body: {city: 'Barcelona'}}
+//       case 'FETCH_LOCATION_SUCCESS':
+//           return {status: 200, body: {city: 'Barcelona'}};
+//       default:
+//         return state;
+//     }
+//   }
 
 export default location;
