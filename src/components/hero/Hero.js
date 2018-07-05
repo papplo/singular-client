618491eccd30a2cb57e7715c7e955f3226f29c49
Pablo Anttila <pablo.anttila@tobii.com>
2018-07-05 11:52:30 +0200

    Feat: now hero has image background and navigation has logo

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,12 +1,23 @@
 import React from 'react';
-
+import BgImage from '../../assets/singular-bg.png';
 export default (props) => {
+
+  const bgImage = {
+    backgroundImage: 'url('+BgImage+')',
+    backgroundSize: 'cover',
+    backgroundPosition: 'bottom center',
+    Transition: 'all',
+    WebkitTransition: 'all', // note the capital 'W' here
+    msTransition: 'all' // 'ms' is the only lowercase vendor prefix
+  };
+
+
   return (
-    <section className={"hero is-medium is-" + props.color + " is-bold"}>
-      <div className="hero-body">
+    <section className={"hero is-medium is-" + props.color + " is-bold"} >
+      <div className="hero-body" style={bgImage}>
         <div className="container">
-          <h1 className="title has-text-centered is-size-2 is-serif has-text-weight-light	"> 
-          {props.userLocation.status === 200 && props.title + props.userLocation.body.city}
+          <h1 className="title has-text-centered is-size-2 is-serif has-text-weight-light	">
+          {props.title} {props.userLocation.status === 200 && props.userLocation.body.city}
           </h1>
         </div>
         <div className="container" style={{paddingTop: 2 + 'em'}}>
